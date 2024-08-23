c3a6ccdd79af4777083ea573733db816b536ce8e
Kynes <kamilpirog98@gmail.com>
2024-08-23 16:10:59 +0200

    chore: import UnityGdsk

---
+++ Assets/PlayFabSDK/MultiplayerAgent/Model/HeartbeatResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace PlayFab.MultiplayerAgent.Model
+{
+    using Helpers;
+    using System;
+
+    [Serializable]
+    public class HeartbeatResponse
+    {
+        [JsonProperty(PropertyName = "sessionConfig")]
+        public SessionConfig SessionConfig { get; set; }
+
+        [JsonProperty(PropertyName = "nextScheduledMaintenanceUtc")]
+        public string NextScheduledMaintenanceUtc { get; set; }
+
+        [JsonProperty(PropertyName = "maintenanceSchedule")]
+        public MaintenanceSchedule MaintenanceSchedule { get; set; }
+
+        [JsonProperty(PropertyName = "operation")]
+        public GameOperation Operation { get; set; }
+    }
+}
